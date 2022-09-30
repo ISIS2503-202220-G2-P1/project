8928f657f672eb09469c5be583960a6f70b6be71
Daniel Reales <da.reales@uniandes.edu.co>
2022-09-30 10:50:49 -0500

    Modificacion

--- avanzo/creditos/views.py
+++ avanzo/creditos/views.py
@@ -3,28 +3,37 @@ from .logic import solicitud_logic as sl
 from django.core import serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SolicitudCredito
+from .serializers import SolicitudCreditoSerializer
 import time
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def solicitud_view(request):
     if request.method == 'GET':
         id_ = request.GET.get('id', None)
         if id_:
             solicitud_dto = sl.get_solicitud(id_)
-            solicitud = serializers.serialize('json', [solicitud_dto,])
-            return HttpResponse(solicitud, 'application/json')
+            solicitud = SolicitudCreditoSerializer(solicitud_dto, many = False)
+            return Response(solicitud.data)
 
         else:
             solicitudes_dto = sl.get_solicitudes()
-            solicitudes = serializers.serialize('json', [solicitudes_dto])
-            return HttpResponse(solicitudes, 'application/json')
+            solicitudes = SolicitudCreditoSerializer(solicitudes_dto, many = True)
+            return Response(solicitudes.data)
 
     if request.method == 'POST':
         # Crea la solicitud
-        solicitud_dto = sl.create_solicitud(json.loads(request.body))
         # Se duerme por 30 segundos simulando el procesamiento
         # Que tendria que hacer Avanzo para determinar si la solicitud
         # es aprobada o no para darle respuesta al usuario
-        time.sleep(30)
-        solicitud = serializers.serialize('json', [solicitud_dto ,])
-        return HttpResponse(solicitud, 'application/json')
+        ser = SolicitudCreditoSerializer(data=request.data) 
+        if ser.is_valid():
+            time.sleep(30)
+            solicitud_dto = sl.create_solicitud(json.loads(request.body))
+            return Response(ser.data, status=status.HTTP_201_CREATED)
+
+        return Response(ser.errors, status = status.HTTP_400_BAD_REQUEST)
