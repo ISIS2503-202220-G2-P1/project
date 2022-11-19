40f2325c85be5f456cc9b799486cefef3c9335f6
Juan Alegría <juanszalegria@gmail.com>
2022-11-19 06:34:00 -0500

    Feat: test get health

--- avanzo/avanzo/views.py
+++ avanzo/avanzo/views.py
@@ -1,10 +1,13 @@
+from django import http   
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 
 
 def healthCheck(request):
-    return HttpResponse('ok')
+    r1 = http.request('GET', 'http://34.70.63.249:8080')
+    r2 = http.request('GET', 'http://35.232.6.147:8080')
+    return HttpResponse(r1+r2)
 
 def index(request):
     return render(request, 'avanzo/index.html')
