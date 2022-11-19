63231aeecd4af6eed75693cdbc0a3be2a9827c97
Juan Alegría <juanszalegria@gmail.com>
2022-11-19 06:50:46 -0500

    Feat: add health animation

--- avanzo/avanzo/views.py
+++ avanzo/avanzo/views.py
@@ -5,9 +5,7 @@ from django.shortcuts import render
 
 
 def healthCheck(request):
-    r1 = http.request('GET', 'http://34.70.63.249:8080')
-    r2 = http.request('GET', 'http://35.232.6.147:8080')
-    return HttpResponse(r1+r2)
+    return render(request, 'avanzo/health.html')
 
 def index(request):
     return render(request, 'avanzo/index.html')
